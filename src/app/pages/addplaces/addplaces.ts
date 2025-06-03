e3fbcb293424f84e89645e400816d13aa4662f17
Kristof Jahnke <jahnke.kristof2@gmail.com>
2025-06-03 16:57:28 +0200

    feat: add savefunction

--- src/app/pages/addplaces/addplaces.ts
+++ src/app/pages/addplaces/addplaces.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-addplaces',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './addplaces.html',
   styleUrl: './addplaces.css'
 })
 export class Addplaces {
+  fb = inject(FormBuilder);
 
+  placeForm = this.fb.group({
+    imageControl: [''],
+    titleControl: [''],
+    categoryControl: [''],
+    descriptionControl: [''],
+    locationControl: [''],
+  })
+
+  logValues(): void {
+    console.log(this.placeForm.value);
+
+    this.placeForm.reset();
+  }
 }
